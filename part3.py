b92e8233606566e83fbcb8c87a18e4636f12204e
Sangeetha <sangeethamg13@gmail.com>
2018-06-23 20:46:08 +0530

    Update part3.py

--- part3.py
+++ part3.py
@@ -6,3 +6,22 @@ from bs4 import BeautifulSoup
 # write your code here
 # usage should be python3 part3.py
 
+url = "https://www.michigandaily.com";
+r = requests.get(url);
+soup = BeautifulSoup(r.text, 'html.parser');
+mostReadList = soup.find("div", {"class": "pane-mostread"});
+mostReadTitles = mostReadList.find_all('a');
+
+print ("Michigan Daily -- MOST READ")
+for title in mostReadTitles:
+    articleName = title.contents[0];
+    articleLink = url + title.get('href');
+    subSoupR = requests.get(articleLink);
+    subSoup = BeautifulSoup(subSoupR.text, 'html.parser');
+    authorLink = subSoup.find("div", {"class": "link"});
+    if(authorLink):
+        author = authorLink.find('a').contents[0]
+    else:
+        author = subSoup.find("p", {"class": "info"}).contents[0];
+    print (articleName)
+    print ("by " + author)
